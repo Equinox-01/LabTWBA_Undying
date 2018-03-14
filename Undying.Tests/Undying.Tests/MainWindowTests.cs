9fe4b621734d3fc2436b1731636054d10cfd4bc6
Equinox <sovereign.stas@yandex.ru>
2018-03-14 04:18:07 +0300

    Unit tests complete.

--- Undying.Tests/Undying.Tests/MainWindowTests.cs
+++ Undying.Tests/Undying.Tests/MainWindowTests.cs
@@ -11,32 +11,54 @@ namespace Undying.Tests
         [TestMethod]
         public void Valid_Data()
         {
-            obj = new Triangle();
-            obj.GetTriangleType();
+            obj = new Triangle("6", "8", "10");
+            var result = obj.GetTriangleType();
+            Assert.IsTrue(result != "");
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
         public void NegativeNumber_Data()
         {
-
+            obj = new Triangle("-3", "-5", "-5");
+            obj.GetTriangleType();
+            Assert.IsTrue(true);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
         public void OverflowNumber_Data()
         {
-
+            obj = new Triangle("10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", "1", "1");
+            obj.GetTriangleType();
+            Assert.IsTrue(true);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(FormatException))]
         public void Symbols_Data()
         {
-
+            obj = new Triangle("aaa", "fff", "ddd");
+            obj.GetTriangleType();
+            Assert.IsTrue(true);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(FormatException))]
         public void Null_Data()
         {
+            obj = new Triangle("", "", "");
+            obj.GetTriangleType();
+            Assert.IsTrue(true);
+        }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidTriangle_Data()
+        {
+            obj = new Triangle("1", "1000", "1");
+            obj.GetTriangleType();
+            Assert.IsTrue(true);
         }
     }
 }
--- Undying/Undying/Triangle.cs
+++ Undying/Undying/Triangle.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Undying
 {
     public class Triangle
     {
+        const
+            double EPS = 0.001d;
+
         private double a_side;
         private double b_side;
         private double c_side;
@@ -24,6 +23,11 @@ namespace Undying
             this.c_side = double.Parse(c_side);
             if ((this.a_side <= 0) || (this.b_side <= 0) || (this.c_side <= 0))
                 throw new ArgumentException("Введённое число меньше либо равно нулю. \nДопустимый промежуток (0; 1,7 × 10^308].");
+            double p = (this.a_side + this.b_side + this.c_side) / 2;
+            if (!(Math.Sqrt(p * (p - this.a_side) * (p - this.b_side) * (p - this.c_side)) < EPS) && 
+                !((this.a_side < this.b_side + this.c_side) && (this.b_side < this.a_side + this.c_side) && (this.c_side < this.a_side + this.b_side)))
+                throw new ArgumentException("Описанный треугольник является вырожденным");
+
         }
 
         public string GetTriangleType()
